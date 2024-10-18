8b09069cb0ec5eab9243a09fd4422f286698bd4e
sam <117124009+haenlonns@users.noreply.github.com>
2024-10-18 07:06:22 -0400

    fix footer fonts

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,22 @@
-import Image from 'next/image'
+import Image from "next/image";
 import Link from "next/link";
 
 export function Footer() {
   return (
-    <footer className="bg-white pr-[855px] pl-[36px] pt-[36px] pb-[65px] flex-col font-figree text-accent">
-        <div className="text-4xl font-extrabold leading-[64px]">
-            Double Degree Club
-        </div>
-        <Link className="pl-4 gap-4 font-mono inline-flex" href="https://www.instagram.com/doubledegreeclub">
-            <Image src="/insta-icon.png" width={24} height={24} alt=""/>
-            <text className="text-base">@doubledegreeclub</text>
-        </Link>
-        <div className="px-4">
-            <u>easter egg</u>
-        </div>
+    <footer className="bg-white pl-[36px] pt-[36px] pb-[65px] flex flex-col">
+      <text className="font-figtree text-header font-extrabold py-4">
+        Double Degree Club
+      </text>
+      <Link
+        className="pl-4 gap-1 inline-flex w-fit"
+        href="https://www.instagram.com/doubledegreeclub"
+      >
+        <Image src="./assets/icons/instagram-icon.svg" width={24} height={24} alt="" />
+        <text className="font-baskerville text-body">@doubledegreeclub</text>
+      </Link>
+      <text className="pl-4 font-baskerville text-body">
+        <u>easter egg!</u>
+      </text>
     </footer>
   );
 }
