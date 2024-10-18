5dd53587884192bf6d701de606e370f96ef4fc2a
sam <117124009+haenlonns@users.noreply.github.com>
2024-10-18 02:56:49 -0400

    temporarily deprecate

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,4 @@
-import Link from "next/link";
-
+/* WIP */
 export function Footer() {
-  return (
-    <footer className="bg-white py-4">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between text-gray-500">
-          <p>Double Degree Club</p>
-          <div className="space-x-4">
-            <Link href="/privacy" className="hover:text-blue-500">
-              Privacy Policy
-            </Link>
-            <Link href="/terms" className="hover:text-blue-500">
-              Terms of Service
-            </Link>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+  return <footer className="bg-white py-4 justify-center"></footer>;
 }
