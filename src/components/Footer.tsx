ac2068edb81c3ac14f1244ea14e2f45c8c42d716
sam <117124009+haenlonns@users.noreply.github.com>
2024-10-18 05:50:08 -0400

    add border

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
 /* WIP */
 export function Footer() {
-  return <footer className="bg-white py-4 justify-center"></footer>;
+  return <footer className="bg-white bordered py-4 justify-center"></footer>;
 }
