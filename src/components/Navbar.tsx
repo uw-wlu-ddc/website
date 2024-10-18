6f07666f3df05a1aaf3bc2ac864c52559784d447
sam <117124009+haenlonns@users.noreply.github.com>
2024-10-18 02:57:03 -0400

    update with new theming

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,27 @@
-import Link from 'next/link';
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
 export function Navbar() {
   return (
-    <div className="relative top-0 items-center h-20 px-6 py-2 font-family bg-white box-border flex font-figtree justify-between shadow border-2 border-purple-950">
-        <Link className="justify-start items-center gap-1 inline-flex pr-12 hover:bg-amber-200" href="/public">
-            <Image className="auto max-w-full" src="./logo/ddc-logo-no-text.svg" width={54} height={56} priority alt=""/>
-            <text className="p-2.5 font-extrabold text-[32px] text-black leading-9">DDC</text>
-        </Link>
-        <div className="h-full text-purple-950 overflow-hidden flex-shrink">
-            <ul className="h-full justify-start items-center inline-flex space-x-6">
-                <li><a className="nav-text" href="/about">About</a></li>
-                <li><a className="nav-text" href="/resources">Resources</a></li>
-                <li><a className="nav-text" href="/constitution">Constitution</a></li>
-                <li><a className="nav-text" href="/execs">Execs</a></li>
-                <li><a className="nav-text" href="/contact">Contact</a></li>
-            </ul>
-        </div>
-    </div>
+    <nav className="fixed w-full z-50 h-20 px-6 py-2 bg-white bordered shadow flex justify-between">
+      <Link className="gap-1 items-center inline-flex" href="/">
+        <Image
+          src="./assets/logo/ddc-logo-no-text.svg"
+          width={54}
+          height={56}
+          priority
+          alt=""
+        />
+        <text className="px-2.5 font-figtree text-header text-black font-extrabold">
+          DDC
+        </text>
+      </Link>
+      <div className="font-figtree text-subheader text-black items-center inline-flex space-x-6">
+        <Link href="/about">About</Link>
+        <Link href="/constitution">Constitution</Link>
+        <Link href="/execs">Execs</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
+    </nav>
   );
 }
