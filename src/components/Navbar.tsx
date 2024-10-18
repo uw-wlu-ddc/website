f72d4436c856e312539662ab97a57f16341cbf5a
Eurphus <p2macdon@uwaterloo.ca>
2024-10-17 21:57:40 -0400

    Navbar Fix, includes temp hovering. Unsure how to style at this time.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,18 +3,17 @@ import Image from 'next/image'
 
 export function Navbar() {
   return (
-    <div className="relative top-0 items-center h-20 px-6 py-2 font-family bg-white box-border flex font-figtree justify-between shadow border-2 border-purple-950">
-        <Link className="justify-start items-center gap-1 inline-flex pr-12 hover:bg-amber-200" href="/public">
-            <Image className="auto max-w-full" src="./logo/ddc-logo-no-text.svg" width={54} height={56} priority alt=""/>
+    <div className="w-full h-[72px] relative top-0 items-center pl-6 pr-9 font-family bg-white box-border flex font-figtree justify-between shadow bordered">
+        <Link className="my-2 justify-start items-center gap-1 inline-flex pr-12" href="/">
+            <Image className="object-contain" src="/logo/ddc-logo-no-text.svg" width={54} height={56} priority alt=""/>
             <text className="p-2.5 font-extrabold text-[32px] text-black leading-9">DDC</text>
         </Link>
-        <div className="h-full text-purple-950 overflow-hidden flex-shrink">
-            <ul className="h-full justify-start items-center inline-flex space-x-6">
-                <li><a className="nav-text" href="/about">About</a></li>
-                <li><a className="nav-text" href="/resources">Resources</a></li>
-                <li><a className="nav-text" href="/constitution">Constitution</a></li>
-                <li><a className="nav-text" href="/execs">Execs</a></li>
-                <li><a className="nav-text" href="/contact">Contact</a></li>
+        <div className="h-full text-accent overflow-hidden flex-shrink">
+            <ul className="h-full justify-start items-center inline-flex gap-6">
+                <li className="h-full items-center justify-center flex"><a className="nav-text" href="/about">About</a></li>
+                <li className="h-full items-center justify-center flex"><a className="nav-text" href="/constitution">Constitution</a></li>
+                <li className="h-full items-center justify-center flex"><a className="nav-text" href="/execs">Execs</a></li>
+                <li className="h-full items-center justify-center flex"><a className="nav-text" href="/contact">Contact</a></li>
             </ul>
         </div>
     </div>
