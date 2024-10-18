3ee7ff0cd19e7f8d79221b2fa9898ef019a3b94a
Eurphus <p2macdon@uwaterloo.ca>
2024-10-18 00:56:33 -0400

    Removed Navbar bordering except for bottom.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export function Navbar() {
   return (
-    <div className="w-full h-[72px] relative top-0 items-center pl-6 pr-9 font-family bg-white box-border flex font-figtree justify-between shadow bordered">
+    <div className="w-full h-[72px] relative top-0 items-center pl-6 pr-9 font-family bg-white box-border flex font-figtree justify-between shadow border-b-2 border-solid border-accent">
         <Link className="my-2 justify-start items-center gap-1 inline-flex pr-12" href="/">
             <Image className="object-contain" src="/logo/ddc-logo-no-text.svg" width={54} height={56} priority alt=""/>
             <text className="p-2.5 font-extrabold text-[32px] text-black leading-9">DDC</text>
