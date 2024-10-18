80204ecd476e3b12f6ab6512a49b6c2996b97d84
sam <117124009+haenlonns@users.noreply.github.com>
2024-10-18 02:57:29 -0400

    add site-wide purple background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-purple">
         <Navbar />
         {children}
         <Footer />
