5838307041094e8ea22bf1eeb28452e1533f1805
sam <117124009+haenlonns@users.noreply.github.com>
2024-10-20 03:07:05 -0400

    added typewriter effect

--- src/app/CardLogo.tsx
+++ src/app/CardLogo.tsx
@@ -2,59 +2,65 @@ import Image from "next/image";
 import ButtonBox from "@/components/ButtonBox";
 
 function ScrollButton() {
-    return (
-        <ButtonBox
-            href="#card-intro"
-            className="absolute left-0 top-0 w-[17.5rem] h-[3.875rem]">
-            <Image
-                src="./assets/small/down-arrow.svg"
-                alt=""
-                width={16}
-                height={21}
-            />
-            <text className="font-figtree text-subheader">Who are we?</text>
-        </ButtonBox>
-    )
+  return (
+    <ButtonBox
+      href="#card-intro"
+      className="absolute left-0 top-0 w-[17.5rem] h-[3.875rem]"
+    >
+      <Image
+        src="./assets/small/down-arrow.svg"
+        alt=""
+        width={16}
+        height={21}
+      />
+      <text className="font-figtree text-subheader">Who are we?</text>
+    </ButtonBox>
+  );
 }
 
 export default function CardLogo() {
-    return (
-        <div className="relative container w-fit mt-[8.5rem] flex bg-white bordered p-[3.5rem]">
-            <div className="items-start my-auto">
-                <Image
-                    src="./assets/logo/ddc-logo-text-yasified.svg"
-                    alt=""
-                    width={476}
-                    height={478}
-                />
-            </div>
-            <div className="ps-[3.125rem] space-y-16">
-                <div className="pe-[0.7rem] space-y-[2.4rem] flex flex-col">
-                    <text className="font-figtree text-title text-black">
-                        Double
-                        <br/>
-                        Degree
-                        <br/>
-                        Club
-                    </text>
-                    <text className="font-dm uppercase text-subtitle text-black">
+  return (
+    <div className="relative container w-fit mt-[8.5rem] flex bg-white bordered p-[3.5rem]">
+      <div className="items-start my-auto">
+        <Image
+          src="./assets/logo/ddc-logo-text-yasified.svg"
+          alt=""
+          width={476}
+          height={478}
+        />
+      </div>
+      <div className="ps-[3.125rem] space-y-16">
+        <div className="pe-[0.7rem] space-y-[2.4rem] flex flex-col">
+          <text className="font-figtree text-title text-black">
+            Double
+            <br />
+            Degree
+            <br />
+            Club
+          </text>
+          {
+          // May Make this into a component later
+          }
+          <div className="relative">
+            <text className="font-dm uppercase text-subtitle text-black before:absolute before:inset-0 before:bg-white before:animate-typewriter after:absolute after:inset-0 after:w-[0.125em] after:animate-caret after:bg-black">
               The best of both worlds
             </text>
           </div>
-                <div className="relative">
-                    <div className="relative left-7 top-7 w-[17.5rem] h-[3.875rem] bg-yellow bordered"/>
-                    <div className="absolute left-4 top-4 w-[17.5rem] h-[3.875rem] bg-black bordered"/>
-                    <ScrollButton />
-                </div>
-            </div>
-            <div className="absolute left-0 top-[110px] translate-x-[-50%] translate-y-[-50%]">
-                <Image
-                    src="/assets/small/faces/home-face-1.svg"
-                    width={160}
-                    height={160}
-                    alt=""
-                />
-            </div>
         </div>
-    );
+        <div className="relative">
+          <div className="relative left-7 top-7 w-[17.5rem] h-[3.875rem] bg-yellow bordered" />
+          <div className="absolute left-4 top-4 w-[17.5rem] h-[3.875rem] bg-black bordered" />
+          <ScrollButton />
+        </div>
+      </div>
+      <div className="absolute left-0 top-[110px] translate-x-[-50%] translate-y-[-50%]">
+        <Image
+          src="/assets/small/faces/home-face-1.svg"
+          width={160}
+          height={160}
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -116,6 +116,38 @@ const config: Config = {
         },
       ],
     },
+    // Typewriter Animation
+    extend: {
+      animation: {
+        typewriter: 'typewriter 2s steps(23) forwards',
+        caret: 'typewriter 2s steps(23) forwards, blink 1s steps(23) infinite 2s',
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: '100%',
+          },
+        },
+        blink: {
+          '0%': {
+            opacity: '0',
+          },
+          '0.1%': {
+            opacity: '1',
+          },
+          '50%': {
+            opacity: '1',
+          },
+          '50.1%': {
+            opacity: '0',
+          },
+          '100%': {
+            opacity: '0',
+          },
+        },
+      },
+    },
+    // End of Animation Code
     container: {
       center: true,
     },
