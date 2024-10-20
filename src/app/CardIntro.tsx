eb222bf1a31467841d7950b1aa91915a203a3406
sam <117124009+haenlonns@users.noreply.github.com>
2024-10-20 00:57:08 -0400

    reformat styling

--- src/app/CardIntro.tsx
+++ src/app/CardIntro.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 
 export default function CardIntro() {
   return (
-    <div className="w-full h-[48rem] pr-[106px] pt-[60px] pb-9 bg flex bg-white bordered justify-center items-center">
+    <div
+      id="card-intro"
+      className="w-full h-fit pr-[6.625rem] pt-[3.75rem] pb-9 bg flex bg-white bordered justify-center items-center"
+    >
       <div className="justify-start items-start inline-flex pt-9 pb-32 pr-[26px] gap-[70px]">
-        <div className="relative w-6/12 min-w-[47rem] h-[31rem] pt-14 pb-[88px] pr-14 pl-[112px] grid bordered gap-8 float-left">
+        <div className="relative w-[47rem] h-[31rem] pt-14 pb-[5rem] pr-14 pl-[7rem] grid bordered gap-8">
           <text className="font-figtree text-header text-black">
             The DDC is an official, UW and WLU registered, student-run
             organization that aims to enhance the Double Degree student
@@ -13,21 +16,24 @@ export default function CardIntro() {
           </text>
           <text className="font-baskerville text-body text-black">
             The club hosts several
-            <b> social events, networking/mentorships opportunities</b>, and
-            <b> academic resources</b>.<b> Our goal </b>
-            is to provide double degrees opportunities to find their community
-            and support.
+            <text className="font-bold">
+              {" "}
+              social events, networking & mentorships opportunities
+            </text>
+            , and
+            <text className="font-bold"> academic resources</text>.
+            <text className="font-bold"> Our goal</text> is to provide double
+            degrees opportunities to find their community and support.
           </text>
           <Link
             className="w-fit inline-block items-center gap-10 py-2 px-4 bg-black-100/50 text-black text-xl font-dm text-subtitle uppercase"
             href="https://www.instagram.com/doubledegreeclub/"
           >
-            view our events on instagram <br />@<u>doubledegreeclub</u>
+            view our events on instagram <br />@
+            <text className="underline">doubledegreeclub</text>
           </Link>
           <div className="absolute top-0 left-[7rem] translate-y-[-50%] py-[1rem] px-[2rem] bg-black">
-            <text className="font-figtree text-header text-white">
-              Intro
-            </text>
+            <text className="font-figtree text-header text-white">Intro</text>
           </div>
           <div className="absolute right-[-22px] bottom-[-10px] translate-y-[50%]">
             <Image
