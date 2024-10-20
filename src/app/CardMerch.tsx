1a1569ac9b62e1451425a609f64cc1178f213df4
sam <117124009+haenlonns@users.noreply.github.com>
2024-10-20 00:57:16 -0400

    add merch hyperlink

--- src/app/CardMerch.tsx
+++ src/app/CardMerch.tsx
@@ -9,7 +9,11 @@ export default function CardMerch() {
           <text className="underline">double degree merch</text>.
         </text>
         <div className="absolute right-[7.125rem] bottom-[-2.188rem] h-[3.875rem] w-[12.688rem] bordered z-11 bg-black"></div>
-        <div className="absolute right-[8.25rem] bottom-[-1.438rem] h-[3.625rem] w-[12rem] bordered z-12 bg-white inline-flex justify-center items-center gap-2">
+        <a
+          href="https://wusa.ca/product/ddc-quarter-zips-2/"
+          target="_blank"
+          className="absolute right-[8.25rem] bottom-[-1.438rem] h-[3.625rem] w-[12rem] bordered z-12 bg-white inline-flex justify-center items-center gap-2"
+        >
           <text className="font-figtree text-subheader">Purchase</text>
           <Image
             src="assets/small/right-arrow.svg"
@@ -17,7 +21,7 @@ export default function CardMerch() {
             width={16}
             height={12}
           ></Image>
-        </div>
+        </a>
       </div>
       <Image
         className="absolute right-0 bottom-0"
