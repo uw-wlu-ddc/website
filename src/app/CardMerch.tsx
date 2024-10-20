d6b8a46fc7a0ec17805dc8a70b77af7f73c5142a
sam <117124009+haenlonns@users.noreply.github.com>
2024-10-20 00:23:04 -0400

    add merch card

---
+++ src/app/CardMerch.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+export default function CardMerch() {
+  return (
+    <div className="container w-[75rem] h-[69rem] flex relative">
+      <div className="bg-white bordered w-[42.5rem] h-fit z-10 p-[3.5rem] relative">
+        <text className="font-figtree text-header text-black">
+          Rep our program! We're proud to announce our latest{" "}
+          <text className="underline">double degree merch</text>.
+        </text>
+        <div className="absolute right-[7.125rem] bottom-[-2.188rem] h-[3.875rem] w-[12.688rem] bordered z-11 bg-black"></div>
+        <div className="absolute right-[8.25rem] bottom-[-1.438rem] h-[3.625rem] w-[12rem] bordered z-12 bg-white inline-flex justify-center items-center gap-2">
+          <text className="font-figtree text-subheader">Purchase</text>
+          <Image
+            src="assets/small/right-arrow.svg"
+            alt=""
+            width={16}
+            height={12}
+          ></Image>
+        </div>
+      </div>
+      <Image
+        className="absolute right-0 bottom-0"
+        src="./assets/merch-graphic.svg"
+        alt=""
+        width={1046}
+        height={1102}
+      />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import CardLogo from "./CardLogo";
 import CardIntro from "./CardIntro"
+import CardMerch from "./CardMerch"
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-[8.5rem]">
       <CardLogo />
       <CardIntro />
-      <div className="w-full h-[1222px] bg-background pl-[148px] pr-[98px] py-[60px]"></div>
+      <CardMerch />
       <div className="w-full h-[695px] px-[344px] pt-[60px] pb-[96px] bordered bg-white"></div>
     </div>
   );
