1c2278b48eb00e6ca72449c181148b26fb70db6a
sam <117124009+haenlonns@users.noreply.github.com>
2024-10-20 00:22:32 -0400

    update import format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { ReactNode } from "react";
 import "../globals.css";
-import { Navbar } from "@/components/Navbar";
-import { Footer } from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Double Degree Club",
