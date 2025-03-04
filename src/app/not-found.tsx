f3ddd8d395cfc480928e1d921f796936bf1b7250
sam <117124009+haenlonns@users.noreply.github.com>
2025-03-04 04:48:42 -0500

    update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,25 @@
 import Image from "next/image";
 import ButtonBox from "@/components/ButtonBox";
 import { Icons } from "@/components/Icons";
+import { TextStyle } from "@/constants/texts";
+import { Routes } from "@/constants/routes";
 
 export default function NotFound() {
   return (
-    <div className="container h-screen w-fit content-center">
-      <div className="relative">
-        <div className="relative grid gap-6 px-12 pt-8 pb-16 bordered bg-white">
-          <span className="font-figtree text-title text-black">404.</span>
-          <text className="font-dm text-subtitle text-black uppercase">
-            This is not the page you are looking for
-          </text>
-        </div>
-        <div className="absolute right-[4.3rem] bottom-[-2.3rem] w-[17.5rem] h-[3.875rem] bordered z-11 bg-black" />
+    <div className="flex h-screen justify-center items-center">
+      <div className="relative flex flex-col gap-6 w-fit px-12 pt-8 pb-16 bordered bg-white text-black">
+        <text className={`${TextStyle.Title}`}>404.</text>
+        <text className={`${TextStyle.Subtitle}`}>
+          This is not the page you are looking for
+        </text>
         <ButtonBox
-          className="absolute right-[5.25rem] bottom-[-1.5rem] w-[17.5rem] h-[3.875rem] z-12 gap-2"
-          href="/"
+          className="absolute right-[5.25rem] bottom-[-1.5rem] w-[17.5rem] h-[3.875rem] z-20 gap-2"
+          href={Routes.Home}
         >
           <Icons.ArrowLeft />
-          <text className="font-figtree text-subheader">Back to home</text>
+          <text className={`${TextStyle.SubHeader}`}>Back to home</text>
         </ButtonBox>
+        <div className="absolute right-[4.3rem] bottom-[-2.3rem] w-[17.5rem] h-[3.875rem] bordered z-10 bg-black" />
       </div>
     </div>
   );
