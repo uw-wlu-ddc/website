c59bec3a60c749fba3bdd7f6e4f0820aff09e159
sam <117124009+haenlonns@users.noreply.github.com>
2025-03-04 03:49:27 -0500

    added more urls

--- src/constants/urls.tsx
+++ src/constants/urls.tsx
@@ -1,4 +1,7 @@
 export const Links = {
     Instagram: "https://www.instagram.com/doubledegreeclub/",
-    Quarterzips: "https://wusa.ca/product/ddc-quarter-zips-2/"
+    Quarterzips: "https://wusa.ca/product/ddc-quarter-zips-2/",
+    Luma: "https://lu.ma/ddclub",
+    CSBBAFAQ: "https://cs.uwaterloo.ca/bbabcs-faq",
+    EasterEgg: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
 }
