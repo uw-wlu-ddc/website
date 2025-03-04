4745aa47ba17304ea76e891ec00b72330b723acd
sam <117124009+haenlonns@users.noreply.github.com>
2025-03-04 02:51:57 -0500

    clean up page

--- src/app/home/CardJoin.tsx
+++ src/app/home/CardJoin.tsx
@@ -1,27 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
 import Label from "@/components/Label";
+import { TextStyle } from "@/constants/texts";
 
 export default function CardJoin() {
-    return (
-        <div
-            className="w-full h-fit px-[21.5rem] py-[3.75rem] bg flex bg-white bordered justify-center items-center"
-        >
-            <div className="relative bordered items-start flex-col container text-center p-[3.5rem]">
-                <text className="text-black text-header">
-                    Joining the DDC exec team is the quickest way to get to know other community-oriented double degrees
-                    and get involved.
-                </text>
-                <div className="my-10">
-                    Placeholder
-                </div>
-                <div className="text-black text-body font-baskerville">
-                    Pay attention to your emails for ddc applications!
-                </div>
-                <Label className="top-0 right-1/2 -translate-y-1/2 translate-x-1/2">
-                    Join DDC!
-                </Label>
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full h-fit py-24 flex justify-center items-center bg-white bordered ">
+      <div className="relative w-[47rem] p-14 gap-y-10 items-center bordered flex flex-col">
+        <Label className="top-0 right-1/2 -translate-y-1/2 translate-x-1/2">
+          Join DDC!
+        </Label>
+        <text className={`${TextStyle.Header} text-black`}>
+          Joining the DDC exec team is the quickest way to get to know other
+          community-oriented double degrees and get involved.
+        </text>
+        <div>Placeholder</div>
+        <text className={`${TextStyle.Body} text-black text-center`}>
+          Pay attention to your emails for DDC applications!
+        </text>
+      </div>
+    </div>
+  );
 }
