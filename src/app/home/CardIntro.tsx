b5b28e3782565a2d0191d5952c18df372a0412f6
sam <117124009+haenlonns@users.noreply.github.com>
2025-03-04 01:35:06 -0500

    cleaned up file

--- src/app/home/CardIntro.tsx
+++ src/app/home/CardIntro.tsx
@@ -1,22 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
 import Label from "@/components/Label";
-import { Icons } from "@/components/Icons";
+import { Faces } from "@/constants/assets";
+import { TextStyle } from "@/constants/texts";
+import { Links } from "@/constants/urls";
 
 export default function CardIntro() {
   return (
     <div
       id="card-intro"
-      className="w-full h-fit pr-[6.625rem] pt-[3.75rem] pb-9 bg flex bg-white bordered justify-center items-center"
+      className="w-full h-fit pr-[6.625rem] pt-[3.75rem] pb-9 flex bg-white bordered justify-center items-center"
     >
       <div className="justify-start items-start inline-flex pt-9 pb-32 pr-[26px] gap-[70px]">
-        <div className="relative w-[47rem] h-[31rem] pt-14 pb-[5rem] pr-14 pl-[7rem] grid bordered gap-8">
-          <text className="font-figtree text-header text-black">
+        <div className="relative w-[47rem] h-[31rem] pt-14 pb-[5rem] pr-14 pl-[7rem] grid gap-8 bordered">
+          <text className={`${TextStyle.Header} text-black`}>
             The DDC is an official, UW and WLU registered, student-run
             organization that aims to enhance the Double Degree student
             experience.
           </text>
-          <text className="font-baskerville text-body text-black">
+          <text className={`${TextStyle.Body} text-black`}>
             The club hosts several
             <text className="font-bold">
               {" "}
@@ -28,16 +30,22 @@ export default function CardIntro() {
             degrees opportunities to find their community and support.
           </text>
           <Link
-            className="w-fit inline-block items-center gap-10 py-2 px-4 bg-black-100/50 text-black text-xl font-dm text-subtitle uppercase"
-            href="https://www.instagram.com/doubledegreeclub/"
+            className={`w-fit inline-block items-center gap-10 py-2 px-4 bg-black-100/50 text-black text-xl ${TextStyle.Subtitle}`}
+            href={Links.Instagram}
           >
-            view our events on instagram <br />@
-            <text className="underline">doubledegreeclub</text>
+            view our events on instagram
+            <br />@<text className="underline">doubledegreeclub</text>
           </Link>
-          <Label className="top-0 left-[7rem] translate-y-[-50%]">Intro</Label>
-          <Icons.Face.Smile className="absolute right-[-22px] bottom-[-10px] translate-y-1/2" />
+          <Label className="top-[-2.25rem] left-[7rem]">Intro</Label>
+          <Image
+            src={Faces.Smile}
+            width={212}
+            height={212}
+            alt=""
+            className="absolute right-[-1.375rem] bottom-[-7.25rem]"
+          />
         </div>
-        <div className="w-auto bordered items-center justify-center float-right whitespace-nowrap flex-grow">
+        <div className="w-auto bordered items-center justify-center">
           Instagram embed placeholder
         </div>
       </div>
