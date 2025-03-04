29b2b9763b22ddaee109d2c410476095277cd4e5
sam <117124009+haenlonns@users.noreply.github.com>
2025-03-04 02:19:51 -0500

    cleaned up page

--- src/app/home/CardMerch.tsx
+++ src/app/home/CardMerch.tsx
@@ -1,24 +1,35 @@
+import Image from "next/image";
 import ButtonBox from "@/components/ButtonBox";
 import { Icons } from "@/components/Icons";
+import { TextStyle } from "@/constants/texts";
+import { Merch } from "@/constants/assets";
+import { Links } from "@/constants/urls";
 
 export default function CardMerch() {
   return (
     <div className="container w-[75rem] h-[69rem] flex relative">
-      <div className="bg-white bordered w-[42.5rem] h-fit z-10 p-[3.5rem] relative">
-        <text className="font-figtree text-header text-black">
+      <div className="relative w-[42.5rem] h-[13.5rem] z-10 p-[3.5rem] bg-white bordered">
+        <text className={`${TextStyle.Header} text-black`}>
           Rep our program! We're proud to announce our latest{" "}
           <text className="underline">double degree merch</text>.
         </text>
-        <div className="absolute right-[7.125rem] bottom-[-2.188rem] h-[3.625rem] w-[12rem] bordered z-11 bg-black" />
         <ButtonBox
-            replaceTab={true}
-            href="https://wusa.ca/product/ddc-quarter-zips-2/"
-            className="absolute right-[8.25rem] bottom-[-1.438rem] h-[3.625rem] w-[12rem] z-12">
-          <text className="font-figtree text-subheader">Purchase</text>
+          replaceTab={true}
+          href={Links.Quarterzips}
+          className="absolute right-[8.25rem] bottom-[-1.438rem] h-[3.625rem] w-[12rem] z-30"
+        >
+          <text className={`${TextStyle.SubHeader} text-black`}>Purchase</text>
           <Icons.ArrowRight />
         </ButtonBox>
+        <div className="absolute right-[7.125rem] bottom-[-2.188rem] h-[3.625rem] w-[12rem] bordered z-20 bg-black" />
       </div>
-      <Icons.Merch.Quarterzips className="absolute right-0 bottom-0" />
+      <Image
+        src={Merch.Quarterzips}
+        width={854}
+        height={900}
+        alt=""
+        className="absolute left-[10.625rem] bottom-0"
+      />
     </div>
   );
 }
