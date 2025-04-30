78b91f3b72e3363e3d25a598bb6de1f9c5e92608
sam <117124009+haenlonns@users.noreply.github.com>
2025-04-30 03:35:45 -0400

    defined constitution page

---
+++ src/app/constitution/page.tsx
@@ -0,0 +1,11 @@
+import ConstitutionText, { frontmatter } from "@/markdown/constitution.mdx";
+
+export default function Constitution() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="px-32 py-16 w-[60rem] flex flex-col items-start bg-white/60 bordered">
+        <ConstitutionText />
+      </div>
+    </div>
+  );
+}
