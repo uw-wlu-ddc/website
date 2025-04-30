c9eb4cbd260d231f55622c277402407f89e81998
sam <117124009+haenlonns@users.noreply.github.com>
2025-04-30 06:53:10 -0400

    fixed asset routes

--- src/constants/assets.tsx
+++ src/constants/assets.tsx
@@ -1,17 +1,17 @@
 export const Faces = {
-    Wink: "./assets/faces/face-wink.svg",
-    Smile: "./assets/faces/face-smile.svg",
+    Wink: "/assets/faces/face-wink.svg",
+    Smile: "/assets/faces/face-smile.svg",
 }
 
 export const DDCLogo = {
-    NoText: "./assets/logos/ddc-logo-no-text.svg",
-    Yassified: "./assets/logos/ddc-logo-text-yasified.svg",
+    NoText: "/assets/logos/ddc-logo-no-text.svg",
+    Yassified: "/assets/logos/ddc-logo-text-yasified.svg",
 }
 
 export const Logo = {
-    Instagram: "./assets/icons/instagram-icon.svg"
+    Instagram: "/assets/icons/instagram-icon.svg"
 }
 
 export const Merch = {
-    Quarterzips: "./assets/images/merch-quarterzips.svg"
+    Quarterzips: "/assets/images/merch-quarterzips.svg"
 }
