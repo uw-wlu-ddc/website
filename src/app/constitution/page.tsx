75e1c9a8b7cc444946a620bdceab2b7dfb99cd1a
Michelle Xu <96635277+xummichelle@users.noreply.github.com>
2025-04-30 01:24:24 -0400

    added constitution page header and its font

---
+++ src/app/constitution/page.tsx
@@ -0,0 +1,9 @@
+export default function Constitution() {
+    return (
+        <div className="items-center flex flex-col ">
+            <div className="page-header headerbox bordered">
+            Constitution
+            </div>
+        </div>
+    );
+}
