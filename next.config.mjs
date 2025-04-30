7e626124bba2313ebf6e171dc257ce28f99c2ca7
sam <117124009+haenlonns@users.noreply.github.com>
2025-04-30 03:45:35 -0400

    remove webpack caching

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,11 @@ import createMDX from '@next/mdx'
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
+  webpack: (config) => {
+    config.cache = false   // ← completely turn off caching
+    return config
+  },
 }
  
 const withMDX = createMDX({
@@ -19,5 +21,4 @@ const withMDX = createMDX({
   },
 })
  
-// Merge MDX config with Next.js config
 export default withMDX(nextConfig)
