da33340feaf1826d8ecff4333b83847ca24f4323
sam <117124009+haenlonns@users.noreply.github.com>
2025-05-01 00:49:57 -0400

    minor reformatting

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -6,7 +6,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     h2: (props: JSX.IntrinsicElements['h2']) => <h2 className="header text-black mt-16" {...props} />,
     h3: (props: JSX.IntrinsicElements['h3']) => <h3 className="subheader text-black" {...props} />,
     h4: (props: JSX.IntrinsicElements['h4']) => <h4 className="subtitle text-black" {...props} />,
-    p:  (props: JSX.IntrinsicElements['p']) => <p  className="body text-black" {...props} />,
+    p:  (props: JSX.IntrinsicElements['p']) => <p className="body text-black" {...props} />,
     ul: (props: JSX.IntrinsicElements['ul']) => <ul  className="body text-black list-inside list-disc" {...props} />,
     ol: (props: JSX.IntrinsicElements['ol']) => <ol  className="body text-black list-outside custom-counter" {...props} />,
     li: (props: JSX.IntrinsicElements['li']) => <li  className="" {...props} />,
