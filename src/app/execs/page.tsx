11e1834fcc4d7449a73980d47f1aed35027c14a7
sam <117124009+haenlonns@users.noreply.github.com>
2025-05-01 02:29:17 -0400

    Initial commit of exex page

---
+++ src/app/execs/page.tsx
@@ -0,0 +1,7 @@
+export default function Executives() {
+  return (
+    <div className="flex flex-col items-center mb-28 ">
+      <p className="mb-16 mt-32 grow-0 page-header headerbox bordered">Executive</p>
+    </div>
+  );
+}
