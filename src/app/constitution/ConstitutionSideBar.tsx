a5ebb1471f06c1c29844be4cb8f434ca6f35f2c6
sam <117124009+haenlonns@users.noreply.github.com>
2025-05-01 00:50:17 -0400

    added sidebar stickiness

--- src/app/constitution/ConstitutionSideBar.tsx
+++ src/app/constitution/ConstitutionSideBar.tsx
@@ -2,7 +2,7 @@ import { ConstitutionRoutes } from "@/constants/routes";
 
 export default function ConstitutionSideBar() {
   return (
-    <div className="relative flex flex-col w-fit h-fit px-8 py-10 items-start mt-24 bg-white/60 bordered">
+    <div className="sticky flex flex-col bottom-0 w-fit h-fit top-40 mt-24 self-start px-8 py-10 bg-white/60 bordered">
       <h3 className="subheader label right-1/2 translate-x-1/2">Contents</h3>
       <ul className="subheader flex flex-col justify-start gap-3">
         <a href={ConstitutionRoutes.Root} className="hover:underline">
