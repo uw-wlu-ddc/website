c6166a574f94dbb4ff7e4612206d12637927ba0d
sam <117124009+haenlonns@users.noreply.github.com>
2025-05-01 00:50:40 -0400

    moved bottom margin to parent

--- src/app/constitution/layout.tsx
+++ src/app/constitution/layout.tsx
@@ -4,13 +4,13 @@ import ConstitutionSideBar from "./ConstitutionSideBar";
 
 export default function ConstitutionLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center mb-28">
       <h1 className="mt-32 mb-16 page-header headerbox bordered">
         Constitution
       </h1>
       <div className="flex item-start gap-7">
         <ConstitutionSideBar />
-        <div className="flex flex-col justify-start mb-28">
+        <div className="flex flex-col justify-start">
           <h4 className="subtitle my-2">Last amended: {frontmatter.date}</h4>
           <div className="px-28 pb-16 w-[58rem] min-h-[28rem] flex flex-col items-start bg-white/60 bordered">
             {children}
