850be39a62b4ca8b47cb785c236942348b8be559
sam <117124009+haenlonns@users.noreply.github.com>
2025-04-30 21:16:35 -0400

    use explicit prop types

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,14 +2,14 @@ import type { MDXComponents } from 'mdx/types'
  
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h1: (props: any) => <h1 className="title text-black" {...props} />,
-    h2: (props: any) => <h2 className="header text-black mt-16" {...props} />,
-    h3: (props: any) => <h3 className="subheader text-black" {...props} />,
-    h4: (props: any) => <h4 className="subtitle text-black" {...props} />,
-    p:  (props: any) => <p  className="body text-black" {...props} />,
-    ul: (props: any) => <ul  className="body text-black list-inside list-disc" {...props} />,
-    ol: (props: any) => <ol  className="body text-black list-outside custom-counter" {...props} />,
-    li: (props: any) => <li  className="" {...props} />,
+    h1: (props: JSX.IntrinsicElements['h1']) => <h1 className="title text-black" {...props} />,
+    h2: (props: JSX.IntrinsicElements['h2']) => <h2 className="header text-black mt-16" {...props} />,
+    h3: (props: JSX.IntrinsicElements['h3']) => <h3 className="subheader text-black" {...props} />,
+    h4: (props: JSX.IntrinsicElements['h4']) => <h4 className="subtitle text-black" {...props} />,
+    p:  (props: JSX.IntrinsicElements['p']) => <p  className="body text-black" {...props} />,
+    ul: (props: JSX.IntrinsicElements['ul']) => <ul  className="body text-black list-inside list-disc" {...props} />,
+    ol: (props: JSX.IntrinsicElements['ol']) => <ol  className="body text-black list-outside custom-counter" {...props} />,
+    li: (props: JSX.IntrinsicElements['li']) => <li  className="" {...props} />,
     ...components,
   }
 }
