d5043933421a7a77fc86611925f310a71475b3b8
sam <117124009+haenlonns@users.noreply.github.com>
2025-05-02 00:38:29 -0400

    clean up footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,10 @@ import { Routes } from "@/constants/routes";
 export default function Footer() {
   return (
     <footer className="bg-white px-[9.25rem] py-16 flex items-center border-t-2 border-black">
-      <div className="flex w-[71.5rem] gap-x-3 text-black">
+      <div className="grid grid-cols-3 w-[71.5rem] gap-x-3 text-black">
         <div className="w-[18.75rem] flex flex-col gap-y-2">
           <p className="subheader">Double Degree Club</p>
-          <ul className="body underline flex flex-col gap-y-2 w-fit">
+          <ul className="body underline flex flex-col w-fit">
             <Link href={Routes.Home}>Home</Link>
             <Link href={Routes.About}>About</Link>
             <Link href={Routes.Constitution}>Constitution</Link>
@@ -18,31 +18,36 @@ export default function Footer() {
             <Link href={Routes.Resources}>Resources</Link>
           </ul>
         </div>
-        <div className="flex flex-col w-[18.75rem] gap-y-2">
+        <div className="w-[18.75rem] flex flex-col gap-y-2">
           <p className="subheader">Connect</p>
-          <div className="body flex flex-col gap-y-2 w-fit">
-            <Link className="inline-flex w-fit" href={Links.Instagram}>
-              <Image src={Logo.Instagram} width={24} height={24} alt="" className="pr-1" />
-              @<p className="underline">doubledegreeclub</p>
+          <ul className="body w-fit flex flex-col">
+            <Link className="flex" href={Links.Instagram}>
+              <Image
+                src={Logo.Instagram}
+                width={24}
+                height={24}
+                alt=""
+                className="pr-1"
+              />
+              @<span className="underline">doubledegreeclub</span>
             </Link>
-            <div>
-              <p className="font-bold">Events: </p>
+            <li>
+              <span className="font-bold">Events: </span>
               <Link href={Links.Luma} className="underline">
                 lu.ma/ddclub
               </Link>
-            </div>
-            <div>
-              <p className="font-bold">Office: </p>
-              <p>M3 1001</p>
-            </div>
-          </div>
+            </li>
+            <li>
+              <span className="font-bold">Office:</span> M3 1001
+            </li>
+          </ul>
         </div>
         <div className="flex flex-col w-[18.75rem] gap-y-2">
           <p className="subheader">Resources</p>
-          <div className="body underline flex flex-col gap-y-2 w-fit">
-            <Link href={Links.CSBBAFAQ}>CS/BBA FAQ</Link>
+          <ul className="body underline flex flex-col w-fit">
+            <Link href={Links.CSBBAFAQ}>CS/BBA and Math/BBA FAQ</Link>
             <Link href={Links.EasterEgg}>Easter egg!</Link>
-          </div>
+          </ul>
         </div>
       </div>
     </footer>
