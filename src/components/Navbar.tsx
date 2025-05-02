c478cb6ac0074298423e63fa7c2c90afccd30302
sam <117124009+haenlonns@users.noreply.github.com>
2025-05-02 00:14:00 -0400

    update border to only bottom

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,22 +5,21 @@ import { DDCLogo } from "@/constants/assets";
 
 export default function Navbar() {
   return (
-    <nav className="fixed w-full top-0 z-50 h-20 px-6 py-2 bg-white bordered shadow flex justify-between">
-      <Link className="h-fit items-center inline-flex" href={Routes.Home}>
-        <Image src={DDCLogo.NoText} width={54} height={56} priority alt="" />
-        <p className="px-4 font-figtree text-header text-black font-extrabold">
-          DDC
-        </p>
-      </Link>
-      <div
-        className="subheader text-black items-center inline-flex gap-x-6"
+    <nav className="fixed w-full top-0 z-50 px-6 py-2 bg-white flex justify-between border-b-2 border-black">
+      <Link
+        className="flex items-center gap-4 header text-black font-extrabold"
+        href={Routes.Home}
       >
+        <Image src={DDCLogo.NoText} width={54} height={56} alt="" />
+        DDC
+      </Link>
+      <ul className="subheader text-black flex items-center gap-x-6">
         <Link href={Routes.Home}>Home</Link>
         <Link href={Routes.About}>About</Link>
         <Link href={Routes.Constitution}>Constitution</Link>
         <Link href={Routes.Execs}>Execs</Link>
-        <Link href={Routes.Resources}>Contact</Link>
-      </div>
+        <Link href={Routes.Resources}>Resources</Link>
+      </ul>
     </nav>
   );
 }
