6198936e574837286ebcae443890f4e271e39ab6
sam <117124009+haenlonns@users.noreply.github.com>
2025-05-20 23:56:59 -0400

    added proper type declaration

--- src/app/home/DisplayTypewriter.tsx
+++ src/app/home/DisplayTypewriter.tsx
@@ -1,5 +1,6 @@
 "use client";
 import TypeIt from "typeit-react";
+import TypeItCore from "typeit";
 
 export default function DisplayTypewriter({ className }: { className?: string }) {
   return (
@@ -12,7 +13,7 @@ export default function DisplayTypewriter({ className }: { className?: string })
         cursorChar: "|",
         deleteSpeed: 50,
       }}
-      getBeforeInit={(instance: any) => {
+      getBeforeInit={(instance: TypeItCore) => {
         instance
           .type("the best in the world")
           .pause(750)
