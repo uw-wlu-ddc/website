77646214290243ac6db451c4dfbb8904a14bfbe9
Michelle Xu <96635277+xummichelle@users.noreply.github.com>
2025-10-11 02:06:18 -0400

    fix type it build error?

--- src/app/home/DisplayTypewriter.tsx
+++ src/app/home/DisplayTypewriter.tsx
@@ -1,27 +1,26 @@
 "use client";
-import TypeIt from "typeit-react";
-import TypeItCore from "typeit";
+import { useEffect, useRef } from "react";
+import TypeIt from "typeit";
 
 export default function DisplayTypewriter({ className }: { className?: string }) {
-  return (
-    <TypeIt
-      as={"h4"}
-      className={className}
-      options={{
+  const elementRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    if (elementRef.current) {
+      new TypeIt(elementRef.current, {
         waitUntilVisible: false,
         speed: 100,
         cursorChar: "|",
         deleteSpeed: 50,
-      }}
-      getBeforeInit={(instance: TypeItCore) => {
-        instance
-          .type("the best in the world")
-          .pause(1500)
-          .delete(12)
-          .pause(1000)
-          .type("of both worlds");
-        return instance;
-      }}
-    />
-  );
+      })
+        .type("the best in the world")
+        .pause(1500)
+        .delete(12)
+        .pause(1000)
+        .type("of both worlds")
+        .go();
+    }
+  }, []);
+
+  return <h4 ref={elementRef} className={className}></h4>;
 }
